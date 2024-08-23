ba492be042cd08597bb564547e830e7683ba719a
yustinbieber <98377062+yustinbieber@users.noreply.github.com>
2024-08-23 20:07:06 -0300

    Update rest.py

--- rest.py
+++ rest.py
@@ -138,5 +138,5 @@ def api_fetch_data():
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
-if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8080)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
